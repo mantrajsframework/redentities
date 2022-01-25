68f8897a09df8ef5070233159128a4750ed9944d
Rafael Gomez Blanes <gomezbl@gmail.com>
2022-01-25 21:42:10 +0100

    .

--- test/postgres/postgres.tests.js
+++ test/postgres/postgres.tests.js
@@ -54,8 +54,7 @@ describe( 'Postgres Redentities tests', () => {
         assert.isTrue( exists );
     });
 
-    /*
-    it( '# Mysql Create schema with one entity', async () => {
+    it( '# Postgres Create schema with one entity', async () => {
         let testSchema = {
             entities: [
                 {
@@ -70,74 +69,73 @@ describe( 'Postgres Redentities tests', () => {
 
          await RedEntities.Entities( testSchema ).CreateSchema();
     });
-    */
 
-    // it( '# Mysql Create schema with multiple entities', async () => {
-    //     let testSchema = {
-    //         entities: [
-    //             {
-    //                 name: EntityShortId(),
-    //                 fields: [
-    //                     { name: "title", type: "string" },
-    //                     { name: "alias", type: "string" }
-    //                 ]
-    //             },
-    //             {
-    //                 name: EntityShortId(),
-    //                 fields: [
-    //                     { name: "user", type: "string" },
-    //                     { name: "password", type: "string" }
-    //                 ]
-    //             },
-    //         ]
-    //     }
+    it( '# Postgres Create schema with multiple entities', async () => {
+        let testSchema = {
+            entities: [
+                {
+                    name: EntityShortId(),
+                    fields: [
+                        { name: "title", type: "string" },
+                        { name: "alias", type: "string" }
+                    ]
+                },
+                {
+                    name: EntityShortId(),
+                    fields: [
+                        { name: "userx", type: "string" },
+                        { name: "password", type: "string" }
+                    ]
+                },
+            ]
+        }
 
-    //     await RedEntities.Entities( testSchema ).CreateSchema();
-    // });
+        await RedEntities.Entities( testSchema ).CreateSchema();
+    });
 
-    // it( '# Mysql GetFieldDefinitionInSchema test', () => {
-    //     let testSchema = {
-    //         entities: [
-    //             {
-    //                 name: "book",
-    //                 fields: [
-    //                     { name: "title", type: "string" },
-    //                     { name: "alias", type: "string" }
-    //                 ]
-    //             }
-    //         ]
-    //     }
+    it( '# Postgres GetFieldDefinitionInSchema test', () => {
+        let testSchema = {
+            entities: [
+                {
+                    name: "book",
+                    fields: [
+                        { name: "title", type: "string" },
+                        { name: "alias", type: "string" }
+                    ]
+                }
+            ]
+        }
 
-    //     let db = RedEntities.Entities(testSchema);
+        let db = RedEntities.Entities(testSchema);
 
-    //     let definition = db.GetFieldDefinitionInSchema( "book", "title" );
+        let definition = db.GetFieldDefinitionInSchema( "book", "title" );
 
-    //     assert.equal( definition.name, "title" );
-    //     assert.equal( definition.type, "string" );
-    // });
+        assert.equal( definition.name, "title" );
+        assert.equal( definition.type, "string" );
+    });
 
-    // it( '# Mysql RenameSchemaEntities test', async () => {
-    //     let testSchema = {
-    //         entities: [
-    //             {
-    //                 name: "book",
-    //                 fields: [
-    //                     { name: "title", type: "string" },
-    //                     { name: "alias", type: "string" }
-    //                 ]
-    //             }
-    //         ]
-    //     }
+    it( '# Postgres RenameSchemaEntities test', async () => {
+        let testSchema = {
+            entities: [
+                {
+                    name: "book",
+                    fields: [
+                        { name: "title", type: "string" },
+                        { name: "alias", type: "string" }
+                    ]
+                }
+            ]
+        }
 
-    //     let re = await RedEntities.Entities(testSchema);
-    //     await re.CreateSchema();
+        let re = await RedEntities.Entities(testSchema);
+        await re.CreateSchema();
 
-    //     await re.RenameSchemaEntities( "t" );
+        await re.RenameSchemaEntities( "t" );
 
-    //     let exists = await re.ExistsTable( "bookt" );
+        let exists = await re.ExistsTable( "bookt" );
 
-    //     assert.isTrue(exists);
-    // });
+        assert.isTrue(exists);
+    });
     
     // it( '# Mysql check entities populated', async () => {
     //     let testSchema = {
